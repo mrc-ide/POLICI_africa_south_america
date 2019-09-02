382179c1950533b0cd52c68c9b297f7633f4e6e0
arranhamlet <arran.hamlet14@imperial.ac.uk>
2019-09-02 13:00:27 +0100

    Updates to plotting and observing events which control input and sliders

--- server.R
+++ server.R
@@ -11,14 +11,14 @@ shinyServer(function(input, output, session){
   output$country_map <- renderLeaflet({res = 300
   country_map_gen(shp1 = shp1,
                   country_of_interest = input$country,
-                  year_of_interest = input$year,
+                  year_of_interest = input$year1,
                   ages_of_interest = isolate(input$age))
   })
   
   'Create endemic zone'
   output$endemic_map <- renderLeaflet({res = 300
   map_made<-endemic_map_gen(shp1 = shp1,
-                            year_of_interest = input$year2,
+                            year_of_interest = input$year3,
                             ages_of_interest = isolate(input$age2))
   map_made
   
@@ -30,15 +30,16 @@ shinyServer(function(input, output, session){
   output$barplot <- renderLeaflet({res = 300
   
   #Subset to country, year and ages of interes
-  country_of_interest <- input$country
-  year_of_interest <- input$year
+  country_of_interest <- input$country2
+  year_of_interest <- input$year2
   
   #Generate df of coverage
-  province <- if(is.null(input$country_df2_rows_selected) || any(input$country_df2_rows_selected == 1)) "all" else input$country_df2_rows_selected - 1
-  
+  province <- "all"#if(is.null(input$country_df2_rows_selected) || any(input$country_df2_rows_selected == 1)) "all" else input$country_df2_rows_selected - 1
+
   plot_data <- coverage_by_age_aggregated(country = unique(shp1[shp1$NAME_0 == country_of_interest, ]$ISO),
                                           province = province,
                                           year = year_of_interest)
+  print(plot_data)
   
   #Plot map
   plot_age_vc_barplot(plot_data)
@@ -50,14 +51,15 @@ shinyServer(function(input, output, session){
   output$linegraph <- renderLeaflet({res = 300
   
   #Subset to country, year and ages of interes
-  country_of_interest <- input$country
-  year_of_interest <- input$year
+  country_of_interest <- input$country2
+  year_of_interest <- input$year2
   
   #Generate df of coverage
   plot_data <- coverage_by_age(country = unique(shp1[shp1$NAME_0 == country_of_interest, ]$ISO),
                                year = year_of_interest)
   #Plot map
   province <- if(is.null(input$country_df2_rows_selected) || any(input$country_df2_rows_selected == 1)) "all" else input$country_df2_rows_selected - 1
+
   plot_age_vc_linegraph(plot_data, province)
   
   })
@@ -70,22 +72,22 @@ shinyServer(function(input, output, session){
   output$country_df <- DT::renderDataTable({
     country_df_gen(shp1 = shp1,
                    country_of_interest = input$country,
-                   year_of_interest = input$year,
+                   year_of_interest = input$year1,
                    ages_of_interest = isolate(input$age))
   })
   
   'Create data.table for country level age exploration'
   output$country_df2 <- DT::renderDataTable({
     country_df_gen(shp1 = shp1,
-                   country_of_interest = input$country,
-                   year_of_interest = input$year,
-                   ages_of_interest = isolate(input$age))
+                   country_of_interest = input$country2,
+                   year_of_interest = input$year2,
+                   ages_of_interest = c(0, 100))
   })
   
   'Create data.table for endemic zone level'
   output$endemic_df <- DT::renderDataTable({
     endemic_df_gen(shp1 = shp1,
-                   year_of_interest = input$year2,
+                   year_of_interest = input$year3,
                    ages_of_interest = isolate(input$age2))
     
   })
@@ -122,39 +124,6 @@ shinyServer(function(input, output, session){
     leafletProxy("endemic_map") %>% clearGroup("endemic_outline")
   })
   
-  observeEvent(input$country_df2_rows_selected, {
-
-    #Subset to country, year and ages of interes
-    country_of_interest <- input$country
-    year_of_interest <- input$year
-    
-    #Generate df of coverage
-    plot_data <- coverage_by_age(country = unique(shp1[shp1$NAME_0 == country_of_interest, ]$ISO),
-                                 year = year_of_interest)
-    #Plot map
-    print(input$country_df_rows_selected)
-    province <- if(is.null(input$country_df2_rows_selected) || any(input$country_df2_rows_selected == 1)) "all" else input$country_df2_rows_selected - 1
-    
-    plot_age_vc_linegraph(plot_data, province)
-
-  })
-  
-  observeEvent(input$country_df2_rows_selected, {
-    #Subset to country, year and ages of interes
-    country_of_interest <- input$country
-    year_of_interest <- input$year
-    
-    #Generate df of coverage
-    province <- if(is.null(input$country_df2_rows_selected) || any(input$country_df2_rows_selected == 1)) "all" else input$country_df2_rows_selected - 1
-    
-    plot_data <- coverage_by_age_aggregated(country = unique(shp1[shp1$NAME_0 == country_of_interest, ]$ISO),
-                                            province = province,
-                                            year = year_of_interest)
-    
-    #Plot map
-    plot_age_vc_barplot(plot_data)
-    
-  })
   
   #Observe clicking the update age range button
   #Plot map
@@ -162,7 +131,7 @@ shinyServer(function(input, output, session){
     output$country_map <- renderLeaflet({res = 300
     country_map_gen(shp1 = shp1,
                     country_of_interest = input$country,
-                    year_of_interest = input$year,
+                    year_of_interest = input$year1,
                     ages_of_interest = isolate(input$age))
     })
   })
@@ -170,12 +139,26 @@ shinyServer(function(input, output, session){
   observeEvent(input$update_range2, {
     output$country_map <- renderLeaflet({res = 300
     map_made<-endemic_map_gen(shp1 = shp1,
-                              year_of_interest = input$year2,
+                              year_of_interest = input$year3,
                               ages_of_interest = isolate(input$age2))
     map_made
     })
   })
   
+  '~~~~~~~~~~~~~ Update inputs based on other inputs ~~~~~~~~~~~~~'
+  observeEvent(input$country, {
+    updateSelectInput(session, "country2", selected = input$country)
+  })
+  observeEvent(input$country2, {
+    updateSelectInput(session, "country", selected = input$country2)
+  })
+  
+  observeEvent(input$year1, {
+    updateSelectInput(session, "year2", selected = input$year1)
+  })
+  observeEvent(input$year2, {
+    updateSelectInput(session, "year1", selected = input$year2)
+  })
 
   '~~~~~~~~~~~~~~~~~~~~~~~ Datatable proxy ~~~~~~~~~~~~~~~~~~~~~~~'
   country_df_proxy <- dataTableProxy('country_df')
@@ -194,20 +177,5 @@ shinyServer(function(input, output, session){
     selectRows(endemic_df_proxy, NULL)
   })
   
-  # 
-  # observeEvent(input$year,{
-  #   selectRows(proxy,input$table_rows_selected)
-  # })
-  # 
-  # observeEvent(input$age,{
-  #   selectRows(proxy,input$table_rows_selected)
-  # })
-  
-  
-  
-  
-  
-  
-  
-  
+
 })
